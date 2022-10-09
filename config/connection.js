a2f2d474c3ef5274089f6e328bd9879e5c0be3f6
alotofsnakes <mrfries@gmail.com>
2022-10-08 17:15:28 -0700

    added missing module.export

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,6 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306
-// });
-// module.exports = sequelize;
-
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -20,3 +13,5 @@ if (process.env.JAWSDB_URL) {
     port: 3306
   });
 }
+
+module.exports = sequelize;
