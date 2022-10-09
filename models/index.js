bfb8239aebabcf4accac2ec3ccf0a9d3e393a1d4
alotofsnakes <mrfries@gmail.com>
2022-10-08 17:01:29 -0700

    added comment routes and model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,23 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -38,4 +55,4 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+module.exports = { User, Post, Vote, Comment };
